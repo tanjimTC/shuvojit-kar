7e6093d5eccb1f64503bd5db0fc160e830082829
tanjimTC <tanjimtc74@gmail.com>
2020-09-08 21:42:55 +0600

    Updates

---
+++ precache-manifest.2f2b95d63ab2501ae0ccffc583051e60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cafa9bfd3728b199724c7a7547017f",
+    "url": "/shuvojit-kar/index.html"
+  },
+  {
+    "revision": "17e4a6b8bf048f5d9de4",
+    "url": "/shuvojit-kar/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "3aeba738495fa805e673",
+    "url": "/shuvojit-kar/static/css/main.54dd28b7.chunk.css"
+  },
+  {
+    "revision": "17e4a6b8bf048f5d9de4",
+    "url": "/shuvojit-kar/static/js/2.f94d44bd.chunk.js"
+  },
+  {
+    "revision": "0e970bdb14e56c5c1be9ae44eeb4dd24",
+    "url": "/shuvojit-kar/static/js/2.f94d44bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aeba738495fa805e673",
+    "url": "/shuvojit-kar/static/js/main.7a994e33.chunk.js"
+  },
+  {
+    "revision": "9738d8100e74a473cbee",
+    "url": "/shuvojit-kar/static/js/runtime-main.bf752806.js"
+  },
+  {
+    "revision": "4e5ca0c87634bbe7aae46219b57bab2d",
+    "url": "/shuvojit-kar/static/media/blog.4e5ca0c8.jpg"
+  },
+  {
+    "revision": "7a473c5b403531f9ee3e5ff586e28310",
+    "url": "/shuvojit-kar/static/media/mypic.7a473c5b.jpg"
+  }
+]);
